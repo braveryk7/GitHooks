119147325949198d1fdee75058b99166ee3b510b
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-10-08 17:36:27 +0900

    refs #13 add function get_files
    get staged file, and filter by extension

---
+++ conf/linter_config.sh
@@ -0,0 +1,29 @@
+function get_files() {
+    if git rev-parse --verify HEAD >/dev/null 2>&1; then
+        against=HEAD
+    else
+        against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+    fi
+
+    exec 1>&2
+
+    files=`git diff-index --cached --name-only --diff-filter=AM ${against}`
+    is_error=0
+    output=""
+
+    php_files=""
+    js_files=""
+    css_files=""
+    for f in ${files}
+        do
+            extension=${f##*.}
+            case ${extension} in
+                php)
+                    php_files+="${f} ";;
+                js | ts | jsx | tsx)
+                    js_files+="${f} ";;
+                css | scss | sass)
+                    css_files+="${f} ";;
+            esac
+    done
+}
